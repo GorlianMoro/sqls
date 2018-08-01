ca0535112d8ab832b19288f103a3ece2956061fd
Maxim <legckostupov.maxim@yandex.ru>
2018-08-01 14:35:14 +0300

    sqls ver. 1.0

--- index.php
+++ index.php
@@ -1,41 +1,39 @@
 <?php
-$jsondata = file_get_contents('data.json') or exit('Не удалось получить данные');
-$data = json_decode($jsondata) or exit('Ошибка декодирования json');
- ?>
+$pdo = new PDO("mysql:host=localhost; dbname=Books", "root", "");
+$sql = "select * from books";
+?>
 
- <!DOCTYPE html>
- <html lang="ru">
-   <head>
-     <meta charset="utf-8">
-     <title>Телефонный справочник</title>
-     <style >
-       table {
-         border: 2px solid black;
-         text-align: center;
-       }
-       td {
-         border: 1px solid black;
-       }
-     </style>
-   </head>
-   <body>
-     <h1>Телефонный справочник</h1>
-     <h2>База данных</h2>
-     <table>
-       <tr>
-         <td>Имя</td>
-         <td>Фамилия</td>
-         <td>Адрес</td>
-         <td>Номер телефона</td>
-       </tr>
-       <?php foreach ($data->persons_data as $dataPerson) { ?>
-         <tr>
-           <td><?php echo (!empty($dataPerson->firstName)) ? $dataPerson->firstName : 'не удалось получить имя'; ?></td>
-           <td><?php echo (!empty($dataPerson->lastName)) ? $dataPerson->lastName : 'не удалось получить фамилию'; ?></td>
-           <td><?php echo (!empty($dataPerson->address)) ? $dataPerson->address : 'не удалось получить адрес'; ?></td>
-           <td><?php echo (!empty($dataPerson->phoneNumber)) ? $dataPerson->phoneNumber : 'не удалось получить телефон'; ?></td>
-         </tr>
-      <?php } ?>
-     </table>
-   </body>
- </html>
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <title>Книги</title>
+    <style>
+      table {
+       border: 2px solid black;
+       text-align: center;
+     }
+    </style>
+  </head>
+  <body>
+    <table>
+      <tr>
+        <td><?php foreach ($pdo->query($sql) as $row) {
+          echo $row['name'] . "<br />";
+        } ?></td>
+        <td><?php foreach ($pdo->query($sql) as $row) {
+          echo $row['author'] . "<br />";
+        } ?></td>
+        <td><?php foreach ($pdo->query($sql) as $row) {
+          echo $row['year'] . "<br />";
+        } ?></td>
+        <td><?php foreach ($pdo->query($sql) as $row) {
+          echo $row['isbn'] . "<br />";
+        } ?></td>
+        <td><?php foreach ($pdo->query($sql) as $row) {
+          echo $row['genre'] . "<br />";
+        } ?></td>
+      </tr>
+    </table>
+  </body>
+</html>
